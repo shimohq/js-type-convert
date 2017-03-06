b85bb829cdcc7d2af418f63ee2dd29f9dcdc5a78
TomWan <wanmingtom@gmail.com>
2017-03-06 21:16:44 +0800

    fix: tests

--- test/test.js
+++ test/test.js
@@ -1,15 +1,21 @@
 'use strict';
 const Nightmare = require('nightmare');
 const should = require('chai').should();
+const path = require('path');
 
 describe('base64 to File', function () {
   this.timeout(15000);
   it('should okay', function (done) {
-    new Nightmare().goto('./test.html').evaluate(function () {
-      return document.getElementById('answer')
-    }, function (result) {
-      result.should.equal('okay');
-      done();
-    }).run();
+    new Nightmare()
+      .goto('file://' + path.join(__dirname, 'test.html'))
+      .evaluate(function () {
+        return document.getElementById('answer').innerHTML;
+      })
+      .end()
+      .then(content => {
+        content.should.equal('okay');
+      })
+      .then(done)
+      .catch(done);
   });
 });
